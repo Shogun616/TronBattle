20d1aebc4b6af6d1a9bf158b2d7ea1236d9bed09
Shogun616 <31846047+Shogun616@users.noreply.github.com>
2017-12-14 21:27:14 +0100

    Add files via upload

--- TronBattle.py
+++ TronBattle.py
@@ -1,84 +1,89 @@
+import math
 import sys
-import random
 
-def move_to_dir(old, new):
-    if old[0] < new[0]:
-        return "RIGHT"
-    if old[1] < new[1]:
-        return "DOWN"
-    if old[0] > new[0]:
-        return "LEFT"
-    else:
-        return "UP"
-    return "SELF DESCTRUCT"
+# Constants:
+WIDTH = 30
+HEIGHT = 20
 
+# Data structure: store the battlefield as a 2D array of size WIDTH x HEIGHT
+# Set all cells to -1 to mark them as empty
+grid = {}
+for i in range(WIDTH):
+   x = []
+   for j in range(HEIGHT):
+       if i < 29:
+           grid.append((i + 1, j))
+       if i > 0:
+           grid.append((i - 1, j))
+       if j < 19:
+           grid.append((i, j + 1))
+       if j > 0:
+           grid.append((i, j - 1))
+   grid[(i, j)] = grid
 
-def get_score(starts):
-    graphs = {i: {} for i in range(n_players)}
-    graphset = set(x for x in occupied)
-    order = list(range(my_id, n_players)) + list(range(0, my_id))
-    it = 1
-    while True:
-        full = True
-        moves = {}
-        for o in order:
-            for x in starts[o]:
-                for n in NEIGHBOURS[x]:
-                    if n not in graphset or (n in moves and it == 1):
-                        full = False
-                        graphset.add(n)
-                        moves[n] = o
-        for k, v in moves.items():
-            graphs[v][k] = it
-        if full:
-            break
-        starts = [[k for k, v in moves.items() if v == i] for i in range(n_players)]
-        it += 1
-    num_my_tiles = len(graphs[my_id])
-    num_enemy_tiles = sum([len(graphs[i]) for i in range(n_players) if i != my_id])
-    enemies_dist = sum([sum(graphs[i].values()) for i in range(n_players) if i != my_id])
-    return sum([num_my_tiles * 10000000, num_enemy_tiles * -100000, enemies_dist])
-
-NEIGHBOURS = {}
-for i in range(30):
-    for j in range(20):
-        neighbours = []
-        if i < 29:
-            neighbours.append((i + 1, j))
-        if i > 0:
-            neighbours.append((i - 1, j))
-        if j < 19:
-            neighbours.append((i, j + 1))
-        if j > 0:
-            neighbours.append((i, j - 1))
-        NEIGHBOURS[(i, j)] = neighbours
-
-occupied = {}
+# loop forever
 while True:
-    n_players, my_id = [int(i) for i in input().split()]
-    curr_moves = []
-    for i in range(n_players):
-        x0, y0, x1, y1 = [int(j) for j in input().split()]
-        occupied[(x0, y0)] = i
-        occupied[(x1, y1)] = i
-        curr_moves.append((x1, y1))
-    for i, cm in enumerate(curr_moves):
-        if cm == (-1, -1):
-            occupied = {k: v for k, v in occupied.items() if v != i}
-    for p in range(n_players):
-        x1, y1 = curr_moves[p]
-        if p == my_id:
-            me = (x1, y1)
-            scores = []
-            for neighbour in NEIGHBOURS[me]:
-                if neighbour not in occupied:
-                    player_starts = [[x] for x in curr_moves.copy()]
-                    player_starts[my_id] = [neighbour]
-                    for i, cm in enumerate(curr_moves):
-                        if cm == (-1, -1):
-                            player_starts[i] = []
-                    score = get_score(player_starts)
-                    scores.append((score, neighbour))
-    best_score_move = sorted(scores, key=lambda x: x[0], reverse=True)[0]
-    print(move_to_dir(me, best_score_move[-1]))
+   # n: total number of players (2 to 4).
+   # p: your player number (0 to 3).
+   n, p = [int(i) for i in input().split()]
+
+   my_x = -1  # X position of my light cycle
+   my_y = -1  # Y position of my light cycle
+
+   # Read in data for where all players are right now:
+   for i in range(n):
+       x0, y0, x1, y1 = [int(j) for j in input().split()]
+       grid[x1][y1] = i
+       if i == p:
+           my_x = x1
+           my_y = y1
+       else:
+           other_player_x = x1
+           other_player_y = y1
+      
+
+   # Write out some debugging output to help us see what is going on:
+   print("Mitt nuvarande x position är {}".format(my_x), file=sys.stderr)
+   print("Mitt nurvarande y position är {}".format(my_y), file=sys.stderr)
+   print("Motståndarens nuvarande x position är {}".format(other_player_x), file=sys.stderr)
+   print("Motståndarens nuvarande y position är {}".format(other_player_y), file=sys.stderr)
+
+   options = []
+   if (my_x + 1) < WIDTH and grid[my_x + 1][my_y] == -1:
+       options.append("RIGHT")
+   if (my_x - 1) >= 0 and grid[my_x - 1][my_y] == -1:
+       options.append("LEFT")
+   if (my_y + 1) < HEIGHT and grid[my_x][my_y + 1] == -1:
+       options.append("DOWN")
+   if (my_y - 1) >= 0 and grid[my_x][my_y - 1] == -1:
+       options.append("UP")
+   if len(options) == 0:
+       print("SELF DESCTRUCT")
+   elif len(options) == 1:
+       print(options[0])
+   else:   
+       if (other_player_x + 1) < WIDTH and grid[other_player_x + 1][other_player_y] == -1:
+           options.append("RIGHT")
+       if (other_player_x - 1) >= 0 and grid[other_player_x - 1][other_player_y] == -1:
+           options.append("LEFT")
+       if (other_player_y + 1) < HEIGHT and grid[other_player_y][other_player_y + 1] == -1:
+           options.append("DOWN")
+       elif (other_player_y - 1) >= 0 and grid[other_player_x][other_player_y - 1] == -1:
+           options.append("UP")
+       else:
+           print("RIGHT")
+          # print("SELF DESTRUCT")
+ 
+       
+
+
+
+
+
+
+
+
+
+
+
 
